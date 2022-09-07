5c0167750b66a00a6d694209652934bb82a304f2
Alesandria Wild <alesandriawild@yahoo.com>
2022-09-06 20:51:10 -0700

    add read me

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function pigLatin(word) {
+let pigVal;
+let wordToArray = word.split('');
+  for(let i = 0; i <=vowel.length; i += 1) {
+    if (wordToArray[0] === vowel[i]){
+      let pigVal = word + "way";
+    }
+  }  
+  let pigValToString = pigVal.toString();
+  return pigValToString;
+};
+
+pigLatin(word);
